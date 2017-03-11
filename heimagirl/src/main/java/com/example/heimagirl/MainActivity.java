7c9c41564fba2e16d0e026c036986806f094a0be
gongyong <295306443@qq.com>
2017-03-11 17:11:54 +0800

    setup buju

---
+++ heimagirl/src/main/java/com/example/heimagirl/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.heimagirl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+
+import com.squareup.okhttp.OkHttpClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.list_view)
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        sendSyncRequest();
+
+    }
+
+    private void sendSyncRequest() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+
+            }
+        }).start();
+
+
+    }
+
+
+}
